ab3851fee57b066312ff3b50ce6752cdd4bae8a4
KatieD8 <63078091+KatieD8@users.noreply.github.com>
2023-10-03 15:10:32 +1300

    added inbedded version

--- dayOne/leapYear.js
+++ dayOne/leapYear.js
@@ -12,3 +12,21 @@ function isLeap(year) {
     
        
 }
+
+or 
+
+function isLeap(year) {
+    if (year%4===0){
+        if (year%100===0){
+            if (year%400 ===0) {
+                return "Leap year."
+            } else {
+                return "Not leap year."
+            }
+        } else { 
+            return "Leap year."
+        }
+    } else {
+        return "Not leap year."
+    }
+}
